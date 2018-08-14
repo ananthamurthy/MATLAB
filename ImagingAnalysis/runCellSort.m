00e13dfe6bf6e62c9cb3df40efbc0f93dcd06ef0
ananthamurthy <kganantha@gmail.com>
2018-08-14 16:48:26 +0530

    implemented CellSort. Now to tweak parameters to best capture my datasets.

--- ImagingAnalysis/runCellSort.m
+++ ImagingAnalysis/runCellSort.m
@@ -27,7 +27,6 @@
 %   movtm - average of all movie pixels at each time frame, after
 %   normalizing each pixel deltaF/F
 
-
 % ICA Inputs:
 %   mixedsig - N x T matrix of N temporal signal mixtures sampled at T
 %   points.
@@ -55,11 +54,16 @@ clear
 
 addpath(genpath('/Users/ananth/Documents/MATLAB/ImagingAnalysis/CellSort'))
 
-dataDir = '/Users/ananth/Desktop/Work/Imaging/';
+%Please edit the following for the dataset to be analysed
+dataset = 'M26_5_1-ROI';
+%NOTE: the dataset directory - dataDirec/dataset/dataset
+%All trials should be concatenated using Fiji and saved as dataset.tif
+
+dataDirec = '/Users/ananth/Desktop/Work/Imaging/';
 outputdir = '/Users/ananth/Desktop/Work/Analysis/Imaging/cellSortOutput/';
 
 %Checks to see if directories are available
-if ~isfolder(dataDir)
+if ~isfolder(dataDirec)
     disp('Data directory not found')
 end
 
@@ -67,38 +71,76 @@ if ~isfolder(outputdir)
     disp('Output directory not found')
 end
 
-%Please edit the following for the dataset to be analysed
-date = '20180508';
-dataset = 'M26_5_1-ROI';
-nTrials = 1; %default: 60
-startTrial = 1;
-%startTrial = nTrials;
-
-% Input Parameters - PCA:
-% Discription - file:///Users/ananth/Documents/MATLAB/ImagingAnalysis/CellSort/doc/CellsortPCA.html
-flims = []; %leave empty
-nPCs = 245; %value optimized after trial run
+%Parameters -
+nPCs = 245;
+mu = 0.6; %Check Mukamel et al., 2009 for details; try the range 0.1 to 0.2
+%nIC = length(PCuse); %PCuse is generated during code run
+%nIC = nPCs;
+%f0 = movm; %movm is generated during code run
+mode = 'series';
+dt = 0.069;
+%ICuse = 1:nIC; %nIC is generated during code run
+smwidth = 0.5;
+thresh = 2.5;
+arealims = 100;
+plotting = 1;
+subtractmean = 1;
+%leave the following empty
+flims = [];
 dsamp = [];
 badframes = [];
-
-% Input Parameters - ICA:
-% Discription - file:///Users/ananth/Documents/MATLAB/ImagingAnalysis/CellSort/doc/CellsortICA.html
-PCuse = [];
-mu = 0.1; %Check Mukamel et al., 2009 for details
-nIC = [];
+ica_A_guess = [];
 termtol = [];
 maxrounds = [];
+tlims = [];
+ratebin = [];
+plottype = [];
+% deconvtau = 0;
+% spike_thresh = 2;
+% normalization = 1;
 
-if isfolder(dataDir)
-    formatSpec = [dataDir, '%s/%s/Trial%s-ROI-1.tif'];
-    for trial = startTrial:nTrials
-        fn = sprintf(formatSpec, date, dataset, num2str(trial));
-        
-        %PCA
-        [mixedsig, mixedfilters, CovEvals, covtrace, movm, movtm] = CellsortPCA(fn, flims, nPCs, dsamp, outputdir, badframes);
-        %Choose PCs
-        
-        %ICA
-        [ica_sig, ica_filters, ica_A, numiter] = CellsortICA(mixedsig, mixedfilters, PCuse, mu, nIC, ica_A_guess, termtol, maxrounds);
-    end
+if isfolder(dataDirec)
+    formatSpec = [dataDirec, '%s/%s.tif'];
+    fn = sprintf(formatSpec, dataset, dataset);
+    
+    %Step1: PCA
+    % Input Parameters - PCA:
+    % Discription - file:///Users/ananth/Documents/MATLAB/ImagingAnalysis/CellSort/doc/CellsortPCA.html
+    [mixedsig, mixedfilters, CovEvals, covtrace, movm, movtm] = CellsortPCA(fn, flims, nPCs, dsamp, outputdir, badframes);
+    
+    %Step2a: Choose PCs
+    fig1 = figure(1);
+    set(fig1,'Position', [1100, 1100, 600, 450]);
+    [PCuse] = CellsortChoosePCs(fn, mixedfilters);
+    
+    %Step2b: Plot PC spctrum
+    CellsortPlotPCspectrum(fn, CovEvals, PCuse)
+    
+    %Step3a: ICA
+    nIC = length(PCuse);
+    % Discription - file:///Users/ananth/Documents/MATLAB/ImagingAnalysis/CellSort/doc/CellsortICA.html
+    [ica_sig, ica_filters, ica_A, numiter] = CellsortICA(mixedsig, mixedfilters, CovEvals, PCuse, mu, nIC, ica_A_guess, termtol, maxrounds);
+    
+    %Step3b: Plot ICA
+    f0 = movm;
+    ICuse = 1:nIC;
+    fig2 = figure(2);
+    set(fig2,'Position', [500, 100, 1200, 800]);
+    %CellsortICAplot(mode, ica_filters, ica_sig, f0, tlims, dt, ratebin, plottype, ICuse, spt, spc)
+    CellsortICAplot(mode, ica_filters, ica_sig, f0, tlims, dt, ratebin, plottype, ICuse)
+    
+    %Step4a: Segmentation
+    [ica_segments, segmentlabel, segcentroid] = CellsortSegmentation(ica_filters, smwidth, thresh, arealims, plotting);
+    
+    %Step4b: Apply filter
+    cell_sig = CellsortApplyFilter(fn, ica_segments, flims, movm, subtractmean);
+    
+    %Step5: Find spikes
+    %[spmat, spt, spc] = CellsortFindspikes(ica_sig, thresh, dt, deconvtau, normalization);
+    
+    %Show results
+    figure(2)
+    %set(fig2,'Position', [500, 100, 600, 450]);
+    CellsortICAplot(mode, ica_filters, ica_sig, f0, tlims, dt, ratebin, plottype, ICuse)
+    
 end
