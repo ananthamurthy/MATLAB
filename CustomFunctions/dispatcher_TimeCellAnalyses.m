0c81048951fa7a14ac5ffe5a0b01e44737a8622f
ananthamurthy <kganantha@gmail.com>
2019-12-24 16:27:02 +0530

    the functions for mehrab's and mau's analysis are ready and the dispatcher seems to be running.

--- CustomFunctions/dispatcher_TimeCellAnalyses.m
+++ CustomFunctions/dispatcher_TimeCellAnalyses.m
@@ -11,9 +11,10 @@ addpath(genpath('/Users/ananth/Documents/MATLAB/ImagingAnalysis/Suite2P-ananth')
 
 %% Dataset
 make_db
-%ops0.method   = 'C'; % A: only PSTH; B: PSTH then filter; C: filter then PSTH; use 'C'
-ops0.fig      = 1;
-ops0.saveData = 1;
+ops0.method          = 'C'; % A: only PSTH; B: PSTH then filter; C: filter then PSTH; use 'C'
+ops0.fig             = 1;
+ops0.saveData        = 1;
+ops0.onlyProbeTrials = 0;
 
 if ops0.fig
     figureDetails = compileFigureDetails(20, 2, 10, 0.5, 'hot'); %(fontSize, lineWidth, markerSize, transparency, colorMap)
@@ -46,22 +47,79 @@ for iexp = 1:length(db)
     realProcessedData = load([saveFolder db(iexp).mouse_name '_' db(iexp).date '_' ops0.method '.mat']);
     trialDetails = getTrialDetails(db(iexp));
     
-    %Mehrab's Reliability Analysis
+    %Significant-Only Traces
+    if ops0.onlyProbeTrials
+        disp('Only analysing Probe Trials ...')
+        dfbf_sigOnly = findSigOnly(realProcessedData.dfbf(:, iProbeTrials, :));
+    else
+        dfbf_sigOnly = findSigOnly(realProcessedData.dfbf);
+    end
+    
+    %Mehrab's Reliability Analysis (Bhalla)
     %Change of hands
     % !! Requires further thought; currently just trying to run the code !!
     learned = 1;
-    blink_list = ones(size(realProcessedData.dfbf,2),1); %Setting blink_list = 1 for all trials
+    blink_list = ones(size(realProcessedData.dfbf, 2), 1); %Setting blink_list = 1 for all trials
     learned_list = [learned_list; learned]; %!! Handle this better !!
     blink_trials = find(blink_list == 1);
+    %Trial specifics
+    CS_onset_frame = trialDetails.frameRate*trialDetails.preDuration + 1;
+    US_onset_frame = floor(trialDetails.frameRate * ...
+        (trialDetails.preDuration + ...
+        trialDetails.csDuration + ...
+        trialDetails.traceDuration)) + 1;
+    frame_time = 1000/trialDetails.frameRate;
     
-    [rrb_ratio_vec_final, timeCells_Mehrab] = runMehrabReliabilityAnalysis(learned_only, bk_period_control, realProcessedData);
+    DATA = dfbf_sigOnly;
+    [rrb_ratio_vec_final, timeCells_Mehrab] = runMehrabReliabilityAnalysis(learned_only, bk_period_control, non_ov_trials, early_only, ...
+        DATA, CS_onset_frame, US_onset_frame, frame_time, r_iters);
     
     %William Mau's Temporal Information (Eichenbaum)
-    [Ispk, timeCells_Eichenbaum] = runMauTIAnalysis(db, ops0, realProcessedData);
+    [Isec, timeCells_Eichenbaum] = runMauTIAnalysis(DATA);
     
     if ops0.save == 1
         save([saveFolder db(iexp).mouse_name '_' db(iexp).date '_multipleAnalysisMethods.mat' ], ...
             'rrb_ratio_vec_final', 'timeCells_Mehrab', ...
             'Ispk', 'timeCells_Eichenbaum')
     end
+    
+    if ops0.fig == 1
+        figure(1)
+        plot(rrb_ratio_vec_final, 'g*')
+        title(['Mehrab - Ridge/Background Ratios | ', ...
+            db(iexp).mouse_name ' ST' num2str(db(iexp).sessionType) ' S' num2str(db(iexp).session) ' | ' ...
+            num2str(trialDetails.frameRate) ' Hz'], ...
+            'FontSize', figureDetails.fontSize, ...
+            'FontWeight', 'bold')
+        xlabel('Cells', ...
+            'FontSize', figureDetails.fontSize,...
+            'FontWeight', 'bold')
+        %set(gca,'YTick',[10, 20, 30, 40, 50, 60])
+        %set(gca,'YTickLabel',({10; 20; 30; 40; 50; 60}))
+        ylabel('Ridge/Background Ratio', ...
+            'FontSize', figureDetails.fontSize,...
+            'FontWeight', 'bold')
+        set(gca,'FontSize', figureDetails.fontSize-3)
+        
+        figure(2)
+        plot(Ispk, 'b*')
+        hold on
+        plot(Isec, 'ro')
+        title(['Eichenbaum - Temporal Information | ', ...
+            db(iexp).mouse_name ' ST' num2str(db(iexp).sessionType) ' S' num2str(db(iexp).session) ' | '...
+            num2str(trialDetails.frameRate) ' Hz | '...
+            '3 frames/bin'] , ...
+            'FontSize', figureDetails.fontSize, ...
+            'FontWeight', 'bold')
+        xlabel('Cells', ...
+            'FontSize', figureDetails.fontSize,...
+            'FontWeight', 'bold')
+        %set(gca,'YTick',[10, 20, 30, 40, 50, 60])
+        %set(gca,'YTickLabel',({10; 20; 30; 40; 50; 60}))
+        ylabel('Information - blue: bits/spike | red: bits/sec)', ...
+            'FontSize', figureDetails.fontSize,...
+            'FontWeight', 'bold')
+        set(gca,'FontSize', figureDetails.fontSize-3)
+    end
 end
+toc
--- CustomFunctions/runMauTIAnalysis.m
+++ CustomFunctions/runMauTIAnalysis.m
@@ -1,37 +1,13 @@
-function [Ispk, timeCells_Eichenbaum] = runMauTIAnalysis(db, ops0, realProcessedData)
-nCells = size(dfbf_sigOnly,1);
+function [Isec, timeCells_Eichenbaum] = runMauTIAnalysis(DATA)
+nCells = size(DATA,1);
 %Preallocations
 MI = zeros(nCells,1);
 Isec = zeros(nCells,1);
 Ispk = zeros(nCells,1);
-Itime = zeros(nCells,size(realProcessedData.dfbf_sigOnly,3),1);
+Itime = zeros(nCells,size(DATA,3),1);
 
-for cell = 1:size(dfbf_sigOnly,1)
-    [MI(cell), Isec(cell), Ispk(cell), Itime(cell, :)] = tempInfoOneNeuron(squeeze(realProcessedData.dfbf_sigOnly(cell,:,:)));
+for cell = 1:size(DATA,1)
+    [MI(cell), Isec(cell), Ispk(cell), Itime(cell, :)] = tempInfoOneNeuron(squeeze(DATA(cell,:,:)));
 end
 
 timeCells_Eichenbaum = []; %Have to populate
-
-%% Plots
-%Information content
-if ops0.fig == 1
-    figure(2)
-    plot(Ispk, 'b*')
-    hold on
-    plot(Isec, 'ro')
-    title(['Eichenbaum - Temporal Information | ', ...
-        db(iexp).mouse_name ' ST' num2str(db(iexp).sessionType) ' S' num2str(db(iexp).session) ' | '...
-        num2str(trialDetails.frameRate) ' Hz | '...
-        '3 frames/bin'] , ...
-        'FontSize', figureDetails.fontSize, ...
-        'FontWeight', 'bold')
-    xlabel('Cells', ...
-        'FontSize', figureDetails.fontSize,...
-        'FontWeight', 'bold')
-    %set(gca,'YTick',[10, 20, 30, 40, 50, 60])
-    %set(gca,'YTickLabel',({10; 20; 30; 40; 50; 60}))
-    ylabel('Information - blue: bits/spike | red: bits/sec)', ...
-        'FontSize', figureDetails.fontSize,...
-        'FontWeight', 'bold')
-    set(gca,'FontSize', figureDetails.fontSize-3)
-end
