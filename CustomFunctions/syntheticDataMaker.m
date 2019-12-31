4e5938cb8698ffc7c4e0f723b89b21e3b725fefd
ananthamurthy <kganantha@gmail.com>
2019-12-31 18:29:51 +0530

    Incorporated many new cases. Now I have to add Noise.

--- CustomFunctions/syntheticDataMaker.m
+++ CustomFunctions/syntheticDataMaker.m
@@ -1,28 +1,38 @@
 %syntheticDataMaker
 %Written by Kambadur Ananthamurthy
-function [syntheticDATA, putativeTimeCells, requiredEventLength] = syntheticDataMaker(DATA, DATA_2D, eventLibrary_2D, ...
+function [syntheticDATA, syntheticDATA_2D, putativeTimeCells, requiredEventLength] = syntheticDataMaker(dataset, DATA, DATA_2D, eventLibrary_2D, ...
     timeCellFraction, cellOrder, ...
     maxHitTrialFraction, trialOrder, ...
     eventSize, eventTiming, ...
-    precision, noise);
+    imprecision, noise)
 syntheticDATA = zeros(size(DATA));
+syntheticDATA_2D = zeros(size(DATA,1),size(DATA,2)*size(DATA,3));
 nTotalCells = size(DATA,1);
+nFrames = dataset.nFrames;
+
+%INFORMATION
+fprintf('... using cell order: %s ...\n', cellOrder)
+fprintf('... using event size: %s ...\n', eventSize)
+fprintf('... using trial order: %s ...\n', trialOrder)
+fprintf('... using event timing: %s ...\n', cellOrder)
+fprintf('... adding noise: %s ...\n', noise)
 
 %Which cells to select?
 putativeTimeCells = zeros(nTotalCells,1);
+nPutativeTimeCells = timeCellFraction * nTotalCells;
 if strcmp(cellOrder, 'basic')
-    nPutativeTimeCells = timeCellFraction * nTotalCells;
     putativeTimeCells(1:nPutativeTimeCells) = 1;
 elseif strcmp(cellOrder, 'random')
-    %TBA
+    r = randi([1 nTotalCells], 1, nPutativeTimeCells);
+    putativeTimeCells(r) = 1;
 end
 
 requiredEventLength = zeros(nTotalCells,1);
 for cell = 1:nTotalCells
+    %fprintf('Cell: %i\n', cell)
     if putativeTimeCells(cell) == 1
         %What size of calcium events to select?
         if strcmp(eventSize, 'max')
-            %disp('Using the largest calcium events ...')
             requiredEventLength(cell) = max(eventLibrary_2D(cell).eventLengths);
         elseif strcmp(eventSize, 'large')
             requiredEventLength(cell) = prctile(eventLibrary_2D(cell).eventLengths, 75);
@@ -34,6 +44,7 @@ for cell = 1:nTotalCells
             %Mix of all sizes of events
             %TBA
         else
+            error('Unknown event size')
         end
     else
         %Skip cell
@@ -64,49 +75,65 @@ for cell = 1:nTotalCells
     %nTotalFrames = size(DATA,3);
     if putativeTimeCells(cell) == 1
         for trial = 1:nTotalTrials
+            count = trial - 1;
+            %fprintf('Count: %i\n', count)
             if hitTrials(trial) == 1
                 %What timing/frame to select?
                 if strcmp(eventTiming, 'basic')
                     %Perfect sequence
-                    frameIndex = cell;
+                    frameIndex = cell + 10; %Avoiding the first 10 frames
+                    %fprintf('frameIndex: %i\n', frameIndex)
                     %Pick event based on requiredEventLength
                     eventNumber = eventLibrary_2D(cell).eventLengths == requiredEventLength(cell);
                     eventStartIndex = eventLibrary_2D(cell).eventStartIndices(eventNumber);
                     event = DATA_2D(cell, eventStartIndex:1:eventStartIndex+requiredEventLength(cell)-1);
                     %Add the effect of "precision"
-                    pad = floor(rand()*precision(1) + floor(rand()*precision(2)));
-                    syntheticDATA(cell, trial, (frameIndex+pad):(frameIndex+pad)+length(event)-1) = event;
-                    
-                    clear eventNumber
-                    clear eventStartIndex
-                    clear event
-                
+                    pad = floor(rand()*imprecision(1) + floor(rand()*imprecision(2)));
+                    syntheticDATA(cell, trial, ((frameIndex+pad):(frameIndex+pad)+length(event)-1)) = event;
+                    %fprintf('syntheticDATA trial length: %i\n', length(syntheticDATA(cell, trial, :)))
                 elseif strcmp(eventTiming, 'clustered')
-                    startFrame = floor(trialDetails.preDuration * trialDetails.frameRate);
-                    endFrame = floor( (trialDetails.preDuration ...
-                        + trialDetails.csDuration ...
-                        + trialDetails.traceDuration) * trialDetails.frameRate);
+                    startFrame = 116;
+                    endFrame = 123;
                     frameIndex = floor(rand()*(endFrame - startFrame)) + startFrame;
+                    %fprintf('frameIndex: %i\n', frameIndex)
                     %Pick event based on requiredEventLength
                     eventNumber = eventLibrary_2D(cell).eventLengths == requiredEventLength(cell);
                     eventStartIndex = eventLibrary_2D(cell).eventStartIndices(eventNumber);
                     event = DATA_2D(cell, eventStartIndex:1:eventStartIndex+requiredEventLength(cell)-1);
                     %Add the effect of "precision"
-                    pad = floor(rand()*precision(1) + floor(rand()*precision(2)));
-                    syntheticDATA(cell, trial, (frameIndex+pad):(frameIndex+pad)+length(event)-1) = event;
-                
+                    pad = floor(rand()*imprecision(1) + floor(rand()*imprecision(2)));
+                    syntheticDATA(cell, trial, ((frameIndex+pad):(frameIndex+pad)+length(event)-1)) = event;
+                    %fprintf('syntheticDATA trial length: %i\n', length(syntheticDATA(cell, trial, :)))
                 elseif strcmp(eventTiming, 'random')
-                    frameIndex = floor(rand()*(size(DATA,3) - 10)); %Avoiding the first 10 frames
+                    startFrame = 10; %Avoiding the first 10 frames
+                    endFrame = nFrames - 80; %Avoiding the last 80 frames
+                    frameIndex = floor(rand()*(endFrame - startFrame)) + startFrame;
+                    %fprintf('frameIndex: %i\n', frameIndex)
                     %Pick event based on requiredEventLength
                     eventNumber = eventLibrary_2D(cell).eventLengths == requiredEventLength(cell);
                     eventStartIndex = eventLibrary_2D(cell).eventStartIndices(eventNumber);
                     event = DATA_2D(cell, eventStartIndex:1:eventStartIndex+requiredEventLength(cell)-1);
                     %Add the effect of "precision"
-                    pad = floor(rand()*precision(1) + floor(rand()*precision(2)));
-                    syntheticDATA(cell, trial, (frameIndex+pad):(frameIndex+pad)+length(event)-1) = event;
+                    pad = floor(rand()*imprecision(1) + floor(rand()*imprecision(2)));
+                    syntheticDATA(cell, trial, ((frameIndex+pad):(frameIndex+pad)+length(event)-1)) = event;
+                    %fprintf('syntheticDATA trial length: %i\n', length(syntheticDATA(cell, trial, :)))
                 end
+                
+                clear eventNumber
+                clear eventStartIndex
+                clear event
+                
+                %Reshape the synthetic data into a 2D matrix
+                syntheticDATA_2D(cell,(((count*nFrames)+1):(count*nFrames)+nFrames)) = syntheticDATA(cell,trial,:);
             else
                 %Skip trial
+                %Reshape the synthetic data into a 2D matrix
+                syntheticDATA_2D(cell,(((count*nFrames)+1):(count*nFrames)+nFrames)) = syntheticDATA(cell,trial,:);
+            end
+            %Add noise (irrespective of hit trial)
+            if strcmp(noise, 'none')
+            elseif strcmp(noise, 'gaussian')
+                %syntheticDATA(cell, trial,:) = awgn((syntheticDATA(cell, trial, :)), 15, 'measured');
             end
         end
     else
