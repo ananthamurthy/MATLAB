73de52a08aa9d2a4dd6bc51c09653599bf9c7601
ananthamurthy <kganantha@gmail.com>
2020-07-18 05:44:50 +0530

    changed "" to '' for Harvest params and added condition to consider trimmed files

--- CustomFunctions/setupHarvestParamsOnServer.m
+++ CustomFunctions/setupHarvestParamsOnServer.m
@@ -1,90 +1,102 @@
 %% SETUP HARVEST PARAMETERS - Method A
 job = 0;
 %1
-% job = job + 1;
-% params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-% params(job).methodList = "A";
-% params(job).sdcpStart = 1;
-% params(job).sdcpEnd = 50;
-% 
-% %2
-% job = job + 1;
-% params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-% params(job).methodList = "A";
-% params(job).sdcpStart = 51;
-% params(job).sdcpEnd = 100;
-% 
-% %3
-% job = job + 1;
-% params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-% params(job).methodList = "A";
-% params(job).sdcpStart = 101;
-% params(job).sdcpEnd = 150;
-% 
-% %4
-% job = job + 1;
-% params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-% params(job).methodList = "A";
-% params(job).sdcpStart = 151;
-% params(job).sdcpEnd = 220;
+job = job + 1;
+params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
+params(job).methodList = 'A';
+params(job).sdcpStart = 1;
+params(job).sdcpEnd = 50;
+params(job).trim = 0;
+
+%2
+job = job + 1;
+params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
+params(job).methodList = 'A';
+params(job).sdcpStart = 51;
+params(job).sdcpEnd = 100;
+params(job).trim = 0;
+
+%3
+job = job + 1;
+params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
+params(job).methodList = 'A';
+params(job).sdcpStart = 101;
+params(job).sdcpEnd = 150;
+params(job).trim = 0;
+
+%4
+job = job + 1;
+params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
+params(job).methodList = 'A';
+params(job).sdcpStart = 151;
+params(job).sdcpEnd = 220;
+params(job).trim = 0;
 
 %% SETUP HARVEST PARAMETERS - Method B
 %5
 job = job + 1;
 params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-params(job).methodList = "B";
+params(job).methodList = 'B';
 params(job).sdcpStart = 1;
 params(job).sdcpEnd = 50;
+params(job).trim = 1;
 
 %6
 job = job + 1;
 params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-params(job).methodList = "B";
+params(job).methodList = 'B';
 params(job).sdcpStart = 51;
 params(job).sdcpEnd = 100;
+params(job).trim = 1;
 
 %7-1
 job = job + 1;
 params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-params(job).methodList = "B";
+params(job).methodList = 'B';
 params(job).sdcpStart = 101;
 params(job).sdcpEnd = 125;
+params(job).trim = 1;
 
 %7-2
 job = job + 1;
 params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-params(job).methodList = "B";
+params(job).methodList = 'B';
 params(job).sdcpStart = 101;
 params(job).sdcpEnd = 125;
+params(job).trim = 1;
 
 %8
 job = job + 1;
 params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-params(job).methodList = "B";
+params(job).methodList = 'B';
 params(job).sdcpStart = 151;
 params(job).sdcpEnd = 220;
+params(job).trim = 1;
 
 %% SETUP HARVEST PARAMETERS - Method C
 % %9
-% job = job + 1;
-% params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-% params(job).methodList = "C";
-% params(job).sdcpStart = 1;
-% params(job).sdcpEnd = 220;
-% 
-% 
-% %% SETUP HARVEST PARAMETERS - Method D
-% %10
-% job = job + 1;
-% params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-% params(job).methodList = "D";
-% params(job).sdcpStart = 1;
-% params(job).sdcpEnd = 220;
+job = job + 1;
+params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
+params(job).methodList = 'C';
+params(job).sdcpStart = 1;
+params(job).sdcpEnd = 220;
+params(job).trim = 0;
+ 
+
+%% SETUP HARVEST PARAMETERS - Method D
+%10
+job = job + 1;
+params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
+params(job).methodList = 'D';
+params(job).sdcpStart = 1;
+params(job).sdcpEnd = 220;
+params(job).trim = 0;
 
 %% SETUP HARVEST PARAMETERS - Method E
 %11
 job = job + 1;
 params(job).fileLocation = sprintf('%s/Imaging/%s/%s/', ANALYSIS_DIR, db.mouseName, db.date);
-params(job).methodList = "E";
+params(job).methodList = 'E';
 params(job).sdcpStart = 1;
 params(job).sdcpEnd = 220;
+params(job).trim = 1;
