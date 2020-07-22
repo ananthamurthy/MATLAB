7ec3860ad9311af00b136ae683272245cb4e40ce
ananthamurthy <kganantha@gmail.com>
2020-07-22 10:51:56 +0530

    now producing some specs on the final data file

--- CustomFunctions/consolidateBatch.m
+++ CustomFunctions/consolidateBatch.m
@@ -61,9 +61,14 @@ for job = 1:length(params)
     end
 end
 
+fullPath4Save = [saveFolder db.mouseName '_' db.date '_synthDataAnalysis_' num2str(date) '_cRun' num2str(cRun) '_cData.mat' ];
+%Information about new file
+h5info(fullPath4Save)
+h5disp(fullPath4Save)
+
 disp('Saving everything ...')
-save([saveFolder db.mouseName '_' db.date '_synthDataAnalysis_' num2str(date) '_cRun' num2str(cRun) '_cData.mat' ], 'cData', '-v7.3')
-%save([saveFolder db.mouseName '_' db.date '_batch' num2str(date) '_run' num2str(run) '_cData.mat' ], 'cData', '-v7')
+save(fullPath4Save, 'cData', '-v7.3')
+%save(fullPath4Save, 'cData', '-v7')
 disp('... done!')
 toc
 disp('All done!')
