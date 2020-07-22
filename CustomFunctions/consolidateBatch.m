e16b32f680a29b3c939aff6c619a2ee0dbace218
ananthamurthy <kganantha@gmail.com>
2020-07-22 12:39:53 +0530

    if this works, the final file structure will be affected by whether there has been any trimming

--- CustomFunctions/consolidateBatch.m
+++ CustomFunctions/consolidateBatch.m
@@ -32,75 +32,75 @@ disp('... done!')
 % cData.methodC = [];
 % cData.methodD = [];
 % cData.methodE = [];
-%% Preallocation
-%Method A
-s.Q = [];
-s.T = [];
-s.timeCells = [];
-s.normQ = [];
-cData.methodA.mAOutput_batch = repmat(s, 1, nDatasets);
-cData.methodA.mAInput = [];
-clear s
-
-%Method B
-s.Mdl = [];
-s.Yfit = [];
-s.Q = [];
-s.trainingTrials = [];
-s.testingTrials = [];
-s.Yfit_actual = [];
-s.YfitDiff = [];
-s.Yfit_2D = [];
-s.Yfit_actual_2D = [];
-s.YfitDiff_2D = [];
-s.timeCells = [];
-s.normQ = [];
-cData.methodB.mBOutput_batch = repmat(s, 1, nDatasets);
-cData.methodB.mBInput = [];
-clear s
-
-%Method C
-s.Q1 = [];
-s.Q2 = [];
-s.T = [];
-s.timeCells = [];
-s.normQ1 = [];
-s.normQ2 = [];
-mCOutput_batch = repmat(s, 1, nDatasets);
-cData.methodC.mCOutput_batch = repmat(s, 1, nDatasets);
-cData.methodC.mCInput = [];
-clear s
-
-%Method D
-s.selectedPC = [];
-s.d1 = [];
-s.dx = [];
-s.dt = [];
-s.Q = [];
-s.T = [];
-s.timeCells = [];
-s.normQ = [];
-cData.methodD.mDOutput_batch = repmat(s, 1, nDatasets);
-cData.methodD.mDInput = [];
-clear s
-
-%Method E
-s.X = [];
-s.X0 = [];
-s.Y = [];
-s.SVMModel = [];
-s.Yfit = [];
-s.YfitDiff = [];
-s.Q = [];
-s.Yfit_2D = [];
-s.Yfit_actual_2D = [];
-s.YfitDiff_2D = [];
-s.Q_2D = [];
-s.T = [];
-s.timeCells = [];
-cData.methodE.mEOutput_batch = repmat(s, 1, nDatasets);
-cData.methodE.mEInput = [];
-clear s
+% %% Preallocation
+% %Method A
+% s.Q = [];
+% s.T = [];
+% s.timeCells = [];
+% s.normQ = [];
+% cData.methodA.mAOutput_batch = repmat(s, 1, nDatasets);
+% cData.methodA.mAInput = [];
+% clear s
+% 
+% %Method B
+% s.Mdl = [];
+% s.Yfit = [];
+% s.Q = [];
+% s.trainingTrials = [];
+% s.testingTrials = [];
+% s.Yfit_actual = [];
+% s.YfitDiff = [];
+% s.Yfit_2D = [];
+% s.Yfit_actual_2D = [];
+% s.YfitDiff_2D = [];
+% s.timeCells = [];
+% s.normQ = [];
+% cData.methodB.mBOutput_batch = repmat(s, 1, nDatasets);
+% cData.methodB.mBInput = [];
+% clear s
+% 
+% %Method C
+% s.Q1 = [];
+% s.Q2 = [];
+% s.T = [];
+% s.timeCells = [];
+% s.normQ1 = [];
+% s.normQ2 = [];
+% mCOutput_batch = repmat(s, 1, nDatasets);
+% cData.methodC.mCOutput_batch = repmat(s, 1, nDatasets);
+% cData.methodC.mCInput = [];
+% clear s
+% 
+% %Method D
+% s.selectedPC = [];
+% s.d1 = [];
+% s.dx = [];
+% s.dt = [];
+% s.Q = [];
+% s.T = [];
+% s.timeCells = [];
+% s.normQ = [];
+% cData.methodD.mDOutput_batch = repmat(s, 1, nDatasets);
+% cData.methodD.mDInput = [];
+% clear s
+% 
+% %Method E
+% s.X = [];
+% s.X0 = [];
+% s.Y = [];
+% s.SVMModel = [];
+% s.Yfit = [];
+% s.YfitDiff = [];
+% s.Q = [];
+% s.Yfit_2D = [];
+% s.Yfit_actual_2D = [];
+% s.YfitDiff_2D = [];
+% s.Q_2D = [];
+% s.T = [];
+% s.timeCells = [];
+% cData.methodE.mEOutput_batch = repmat(s, 1, nDatasets);
+% cData.methodE.mEInput = [];
+% clear s
 
 for job = 1:length(params)
     fprintf('Parsing output from job: %i\n', job)
@@ -110,40 +110,40 @@ for job = 1:length(params)
             cData.methodA.mAInput = jobData.mAInput;
             cData.methodA.mAOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.mAOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         else
-            cData.methodA.mAInput = jobData.holyData.mAInput;
-            cData.methodA.mAOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mAOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
+            cData.methodA.holyData.mAInput = jobData.holyData.mAInput;
+            cData.methodA.holyData.mAOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mAOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         end
     elseif strcmpi(params(job).methodList, 'B')
         if ~params(job).trim
             cData.methodB.mBInput = jobData.mBInput;
             cData.methodB.mBOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.mBOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         else
-            cData.methodB.mBInput = jobData.holyData.mBInput;
-            cData.methodB.mBOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mBOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
+            cData.methodB.holyData.mBInput = jobData.holyData.mBInput;
+            cData.methodB.holyData.mBOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mBOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         end
     elseif strcmpi(params(job).methodList, 'C')
         if ~params(job).trim
             cData.methodC.mCInput = jobData.mCInput;
             cData.methodC.mCOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.mCOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         else
-            cData.methodC.mCInput = jobData.holyData.mCInput;
-            cData.methodC.mCOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mCOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
+            cData.methodC.holyData.mCInput = jobData.holyData.mCInput;
+            cData.methodC.holyData.mCOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mCOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         end
     elseif strcmpi(params(job).methodList, 'D')
         if ~params(job).trim
             cData.methodD.mDInput = jobData.mDInput;
             cData.methodD.mDOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.mDOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         else
-            cData.methodD.mDInput = jobData.holyData.mDInput;
-            cData.methodD.mDOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mDOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
+            cData.methodD.holyData.mDInput = jobData.holyData.mDInput;
+            cData.methodD.holyData.mDOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mDOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         end
     elseif strcmpi(params(job).methodList, 'E')
         if ~params(job).trim
             cData.methodE.mEInput = jobData.mEInput;
             cData.methodE.mEOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.mEOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         else
-            cData.methodE.mEInput = jobData.holyData.mEInput;
-            cData.methodE.mEOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mEOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
+            cData.methodE.holyData.mEInput = jobData.holyData.mEInput;
+            cData.methodE.holyData.mEOutput_batch(params(job).sdcpStart:params(job).sdcpEnd) = jobData.holyData.mEOutput_batch(params(job).sdcpStart:params(job).sdcpEnd);
         end
     else
     end
