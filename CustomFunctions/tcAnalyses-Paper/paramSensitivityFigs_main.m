b2325406c97e0c394287fc6fd15c1407b7ad1e05
ananthamurthy <kganantha@gmail.com>
2020-10-24 07:17:29 +0530

    added a new method. testing..

--- CustomFunctions/tcAnalyses-Paper/paramSensitivityFigs_main.m
+++ CustomFunctions/tcAnalyses-Paper/paramSensitivityFigs_main.m
@@ -9,30 +9,30 @@ labels.xtitle
 labels.ytitle
 %}
 plotSyntheticData = 1;
-plotAnalysedData = 0;
+plotAnalysedData = 1;
 
 addpath(genpath('/Users/ananth/Documents/MATLAB/CustomFunctions'))
 
 %% Load Generated Synthetic Data
 if plotSyntheticData
-    gDate = 20201006; %generation date
-    gRun = 11; %generation run number
-    nDatasets = 14;
+    gDate = 20201008; %generation date
+    gRun = 1; %generation run number
+    nDatasets = 208;
     synthDataFilePath = sprintf('/Users/ananth/Desktop/Work/Analysis/Imaging/M26/20180514/synthDATA_%i_gRun%i_batch_%i.mat', gDate, gRun, nDatasets);
     load(synthDataFilePath)
 end
 
 %% Load Analysed Data
 if plotAnalysedData
-    cDate = 20200729; %consolidation date
+    cDate = 20201010; %consolidation date
     cRun = 1; %consolidation run number
     analysisFilePath = sprintf('/Users/ananth/Desktop/Work/Analysis/Imaging/M26/20180514/M26_20180514_synthDataAnalysis_%i_cRun%i_cData.mat', cDate, cRun);
     load(analysisFilePath)
 else
     cData = [];
 end
-% %% Q vs HTR
-% 
+%% Q vs HTR
+
 % %dIndices = 1:1:10;
 % dIndices = 21:1:30;
 % normalize = 1;
@@ -72,7 +72,7 @@ end
 % plotParamSensitivityLinePlot2(dIndices, normalize, labels, figureDetails, sdo_batch, cData, nMethods, nParams)
 % mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
 % 
-% dIndices = 131:1:140;
+% dIndices = 125:1:134;
 % normalize = 1;
 % nParams = 10;
 % if plotAnalysedData
@@ -91,7 +91,7 @@ end
 % plotParamSensitivityLinePlot2(dIndices, normalize, labels, figureDetails, sdo_batch, cData, nMethods, nParams)
 % mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
 % 
-% dIndices = 141:1:150;
+% dIndices = 135:1:144;
 % normalize = 1;
 % nParams = 10;
 % if plotAnalysedData
@@ -112,7 +112,7 @@ end
 % 
 % %% Q vs Noise
 % 
-% dIndices = 101:1:110;
+% dIndices = 95:1:104;
 % normalize = 1;
 % nParams = 10;
 % if plotAnalysedData
@@ -131,7 +131,7 @@ end
 % plotParamSensitivityLinePlot2(dIndices, normalize, labels, figureDetails, sdo_batch, cData, nMethods, nParams)
 % mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
 % 
-% dIndices = 211:1:220;
+% dIndices = 199:1:208;
 % normalize = 1;
 % nParams = 10;
 % if plotAnalysedData
@@ -152,8 +152,8 @@ end
 
 %% Q vs Event Widths
 
-dIndices = 1:1:7;
-%dIndices = 61:1:67;
+%dIndices = 1:1:7;
+dIndices = 61:1:67;
 normalize = 1;
 nParams = 7;
 if plotAnalysedData
@@ -170,10 +170,10 @@ labels.ytitle = 'Normalized Quality';
 figureDetails = compileFigureDetails(16, 2, 10, 0.5, 'jet'); %(fontSize, lineWidth, markerSize, transparency, colorMap)
 %plotParamSensitivity(dIndices, normalize, labels, figureDetails, sdo_batch, cData)
 plotParamSensitivityLinePlot2(dIndices, normalize, labels, figureDetails, sdo_batch, cData, nMethods, nParams)
-%mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
+mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
 
-dIndices = 8:1:14;
-%dIndices = 68:1:74;
+%dIndices = 8:1:14;
+dIndices = 68:1:74;
 normalize = 1;
 nParams = 7;
 if plotAnalysedData
@@ -185,53 +185,53 @@ labels.xtickscell = {'10', '20', '30', '40', '50', '60', '70', '80', '90', '100'
 labels.titleMain = 'Sequential Activity';
 labels.titlePTC = 'Putative Time Cells';
 labels.titleOC = 'Other Time Cells';
-labels.xtitle = sprintf('Variable Event Width Percentile (%i stddev)', sdcp(8).eventWidth{2});
+labels.xtitle = sprintf('Variable Event Width Percentile (%i stddev)', sdcp(68).eventWidth{2});
 labels.ytitle = 'Normalized Quality';
 figureDetails = compileFigureDetails(16, 2, 10, 0.5, 'jet'); %(fontSize, lineWidth, markerSize, transparency, colorMap)
 %plotParamSensitivity(dIndices, normalize, labels, figureDetails, sdo_batch, cData)
 plotParamSensitivityLinePlot2(dIndices, normalize, labels, figureDetails, sdo_batch, cData, nMethods, nParams)
-%mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
+mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
 
-% dIndices = 165:1:171;
-% normalize = 1;
-% nParams = 7;
-% if plotAnalysedData
-%     nMethods = 7;
-% else
-%     nMethods = 1;
-% end
-% labels.xtickscell = {'10', '20', '30', '40', '50', '60', '70', '80', '90', '100'};
-% labels.titleMain = 'Random Activity';
-% labels.titlePTC = 'Putative Time Cells';
-% labels.titleOC = 'Other Time Cells';
-% labels.xtitle = 'Fixed Event Width Percentile';
-% labels.ytitle = 'Normalized Quality';
-% figureDetails = compileFigureDetails(16, 2, 10, 0.5, 'jet'); %(fontSize, lineWidth, markerSize, transparency, colorMap)
-% %plotParamSensitivity(dIndices, normalize, labels, figureDetails, sdo_batch, cData)
-% plotParamSensitivityLinePlot2(dIndices, normalize, labels, figureDetails, sdo_batch, cData, nMethods, nParams)
-% mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
-% 
-% dIndices = 172:1:178;
-% normalize = 1;
-% nParams = 7;
-% if plotAnalysedData
-%     nMethods = 7;
-% else
-%     nMethods = 1;
-% end
-% labels.xtickscell = {'10', '20', '30', '40', '50', '60', '70', '80', '90', '100'};
-% labels.titleMain = 'Random Activity';
-% labels.titlePTC = 'Putative Time Cells';
-% labels.titleOC = 'Other Time Cells';
-% labels.xtitle = sprintf('Variable Event Width Percentile (%i stddev)', sdcp(181).eventWidth{2});
-% labels.ytitle = 'Normalized Quality';
-% figureDetails = compileFigureDetails(16, 2, 10, 0.5, 'jet'); %(fontSize, lineWidth, markerSize, transparency, colorMap)
-% %plotParamSensitivity(dIndices, normalize, labels, figureDetails, sdo_batch, cData)
-% plotParamSensitivityLinePlot2(dIndices, normalize, labels, figureDetails, sdo_batch, cData, nMethods, nParams)
-% mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
-% 
-% %% Q vs Imprecision
-% 
+dIndices = 165:1:171;
+normalize = 1;
+nParams = 7;
+if plotAnalysedData
+    nMethods = 7;
+else
+    nMethods = 1;
+end
+labels.xtickscell = {'10', '20', '30', '40', '50', '60', '70', '80', '90', '100'};
+labels.titleMain = 'Random Activity';
+labels.titlePTC = 'Putative Time Cells';
+labels.titleOC = 'Other Time Cells';
+labels.xtitle = 'Fixed Event Width Percentile';
+labels.ytitle = 'Normalized Quality';
+figureDetails = compileFigureDetails(16, 2, 10, 0.5, 'jet'); %(fontSize, lineWidth, markerSize, transparency, colorMap)
+%plotParamSensitivity(dIndices, normalize, labels, figureDetails, sdo_batch, cData)
+plotParamSensitivityLinePlot2(dIndices, normalize, labels, figureDetails, sdo_batch, cData, nMethods, nParams)
+mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
+
+dIndices = 172:1:178;
+normalize = 1;
+nParams = 7;
+if plotAnalysedData
+    nMethods = 7;
+else
+    nMethods = 1;
+end
+labels.xtickscell = {'10', '20', '30', '40', '50', '60', '70', '80', '90', '100'};
+labels.titleMain = 'Random Activity';
+labels.titlePTC = 'Putative Time Cells';
+labels.titleOC = 'Other Time Cells';
+labels.xtitle = sprintf('Variable Event Width Percentile (%i stddev)', sdcp(172).eventWidth{2});
+labels.ytitle = 'Normalized Quality';
+figureDetails = compileFigureDetails(16, 2, 10, 0.5, 'jet'); %(fontSize, lineWidth, markerSize, transparency, colorMap)
+%plotParamSensitivity(dIndices, normalize, labels, figureDetails, sdo_batch, cData)
+plotParamSensitivityLinePlot2(dIndices, normalize, labels, figureDetails, sdo_batch, cData, nMethods, nParams)
+mainDatasetCheck3(dIndices, sdo_batch, figureDetails)
+
+%% Q vs Imprecision
+
 % dIndices = 75:1:84;
 % normalize = 1;
 % nParams = 10;
